d631196465d6e8970cc09a3f23499f5b41e1f8a8
Raj malviya <rajm45928@gmail.com>
2025-10-15 17:53:11 +0530

    Add files via upload

--- indexDay37.js
+++ indexDay37.js
@@ -12,6 +12,14 @@ app.get("/", (req, res ) => {
     res.render("home.ejs");
 });
 
+app.get("/ig/:username", (req, res) => {
+    let {username} =  req.params;
+    const instaData = require("./data.json");
+    const data = instaData[username];
+    console.log(data);
+        res.render("instagram.ejs", {data });
+});
+
 app.get("/hello", (req, res ) => {
     res.send("hello");
 });
